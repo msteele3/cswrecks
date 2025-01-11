c326c1b2b1531de63cd51f173765d84490b245e1
msteele3 <mpsteele553@gmail.com>
2025-01-10 22:37:28 -0500

    mobile adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -16,9 +16,8 @@ export default function HomePage() {
     );
   });
 
-
   function chunkSites(sites: Site[]) {
-    const chunkPattern = [4, 5]; 
+    const chunkPattern = [4, 5];
     const result: Site[][] = [];
     let index = 0;
     let i = 0;
@@ -54,7 +53,6 @@ export default function HomePage() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.6 }}
-
       style={{ fontFamily: "'Nova Square', sans-serif" }}
       className="min-h-screen p-8 flex flex-col items-center bg-navy text-white transition-colors duration-300"
     >
@@ -72,7 +70,7 @@ export default function HomePage() {
         initial={{ opacity: 0, y: 20 }}
         animate={{ opacity: 1, y: 0 }}
         transition={{ delay: 0.2 }}
-        className="w-full max-w-2xl my-8"
+        className="w-full max-w-2xl my-8 px-4"
       >
         <div className="relative">
           <input
@@ -82,7 +80,7 @@ export default function HomePage() {
             onChange={(e) => setSearch(e.target.value)}
             className="w-full px-8 py-2 focus:outline-none text-sm border-b transition-colors duration-300 bg-black border-gray-700 text-gray-300 placeholder-gray-600"
           />
-          <div className="absolute left-0 top-2.5">
+          <div className="absolute left-0 top-2.5 pl-1">
             <svg
               className="w-4 h-4 text-gray-600"
               fill="none"
@@ -100,10 +98,14 @@ export default function HomePage() {
         variants={container}
         initial="hidden"
         animate="show"
-        className="w-full max-w-4xl flex flex-col items-center"
+        className="w-full max-w-4xl flex flex-col items-center px-4"
       >
         {chunkedSites.map((rowSites, rowIndex) => (
-          <div key={rowIndex} className="flex gap-x-10 mb-12 justify-center">
+          <div
+            key={rowIndex}
+            className="flex flex-wrap sm:flex-nowrap gap-8 justify-center 
+                       mb-6 sm:mb-12 text-center"
+          >
             {rowSites.map((site) => (
               <motion.a
                 whileHover={{
@@ -115,13 +117,12 @@ export default function HomePage() {
                 href={site.website.replace(/\/$/, "")}
                 target="_blank"
                 rel="noopener noreferrer"
-                className="inline-block text-sm text-gray-300 hover:text-gray-100"
+                className="px-1 inline-block w-1/2 sm:w-auto text-md sm:text-sm text-gray-300 hover:text-gray-100"
               >
                 {site.website
                   .replace("https://", "")
                   .replace("http://", "")
                   .replace(/^www\./, "")
-
                   .replace(/\/$/, "")}
               </motion.a>
             ))}
@@ -129,16 +130,18 @@ export default function HomePage() {
         ))}
       </motion.div>
 
-      <footer className="absolute bottom-10 justify-center mt-12">
+      <footer className="relative bottom-0 mt-12">
         <motion.button
-         whileHover={{
-          scale: 1.15,
-          transition: { duration: 0.15}
-        }}
+          whileHover={{
+            scale: 1.15,
+            transition: { duration: 0.15 },
+          }}
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          onClick={() => window.open("https://github.com/msteele3/cswrecks/pulls", "_blank")}
-          className="text-sm px-6 py-2 rounded-full border transition-colors duration-200 text-white border-gray-800"
+          onClick={() =>
+            window.open("https://github.com/msteele3/cswrecks/pulls", "_blank")
+          }
+          className="text-lg sm:text-sm px-6 py-2 rounded-full border transition-colors duration-200 text-white border-gray-800"
         >
           add your site →
         </motion.button>
