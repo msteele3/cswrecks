84264f722f8b4aff72eeb3499586498f286a518c
Race Williams <race@racewilliams.com>
2025-01-13 08:05:38 -0500

    append to allSites

--- app/sites.ts
+++ app/sites.ts
@@ -1,50 +1,55 @@
 // data/sites.ts
 
 export interface Site {
-    name: string;
-    year: number;
-    website: string;
+  name: string;
+  year: number;
+  website: string;
+}
+
+export const allSites: Site[] = [
+  {
+    name: "Matt Steele",
+    year: 2025,
+    website: "https://www.mattsteele.co/",
+  },
+  {
+    name: "Ben Steele",
+    year: 2025,
+    website: "https://www.bensteele.net/",
+  },
+  {
+    name: "Ananth Vivekanand",
+    year: 2025,
+    website: "https://ananthvivekanand.com/",
+  },
+  {
+    name: "Alex Wang",
+    year: 2027,
+    website: "https://www.alexwang.info/",
+  },
+  {
+    name: "Varun Patel",
+    year: 2027,
+    website: "https://www.bluehandcoding.com/"
+  },
+  {
+    name: "Austin Morris",
+    year: 2024,
+    website: "https://www.auwimo.com/"
+  },
+  {
+    name: "Noah Buchanan",
+    year: 2028,
+    website: "https://noahbuchanan.me"
+  },
+  {
+    name: "Aidan Pratt",
+    year: 2025,
+    website: "https://aidanpratt.com/"
+  },
+  {
+    name: "Race Williams",
+    year: 2026,
+    website: "https://raquent.in"
   }
-  
-  export const allSites: Site[] = [
-    {
-      name: "Matt Steele",
-      year: 2025,
-      website: "https://www.mattsteele.co/",
-    },
-    {
-        name: "Ben Steele",
-        year: 2025,
-        website: "https://www.bensteele.net/",
-      },
-      {
-        name: "Ananth Vivekanand",
-        year: 2025,
-        website: "https://ananthvivekanand.com/",
-      },
-      {
-        name: "Alex Wang",
-        year: 2027,
-        website: "https://www.alexwang.info/",
-      },
-      {
-        name: "Varun Patel",
-        year: 2027,
-        website: "https://www.bluehandcoding.com/"
-      },
-      {
-        name: "Austin Morris",
-        year: 2024,
-        website: "https://www.auwimo.com/"
-      },
-      {
-          name: "Noah Buchanan",
-          year: 2028,
-          website: "https://noahbuchanan.me"
-      },
-      {
-        name: "Aidan Pratt",
-        year: 2025,
-        website: "https://aidanpratt.com/"
-      }
-  ];
+];
