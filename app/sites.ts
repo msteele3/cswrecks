e2add419a12ed7b97b7367d7a2c47d7bcb433d56
Shreyas Gadagi <149131804+Shreyas-Gadagi@users.noreply.github.com>
2025-03-28 17:53:45 -0400

    Update sites.ts

--- app/sites.ts
+++ app/sites.ts
@@ -192,10 +192,15 @@ export const allSites: Site[] = [
     year: 2027,
     website: "https://www.jeetdekivadia.co/"
   },
-	{
-		name: "Vignesh Suresh Kumar",
-		year: 2026,
-		website: "https://vigneshsk17.github.io/"
-	}
+{
+	name: "Vignesh Suresh Kumar",
+	year: 2026,
+	website: "https://vigneshsk17.github.io/"
+},
+{
+  name: "Shreyas Gadagi",
+  year: 2026, // e.g., graduation year
+  website: "https://shreyasgadagi.vercel.app/"
+}
 
 ];
